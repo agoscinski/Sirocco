44a39259c76ae1c6d6786ac7dbdd1d45c5d14d08
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-06-13 18:47:40 +0200

    add run workflow

--- ci/preparerun.py
+++ ci/preparerun.py
@@ -1,8 +1,9 @@
 # DON
 
-import os
 import shutil
 from pathlib import Path
+from sirocco.core import Workflow
+from sirocco.workgraph import AiidaWorkGraph
 
 test_case = Path("../tests/cases/small-icon/").absolute()
 workdir = Path("../workdir").absolute()
@@ -14,6 +15,11 @@ yml_path = workdir / "config/config.yml"
 yml_path.read_text().replace("/TESTS_ROOTDIR", str(workdir))
 yml_path.read_text().replace("/REMOTE_DATADIR", str(workdir))
 # TODO maybe need to copy remote datadir
+
+core_workflow = Workflow.from_config_file(workdir / "config.yml")
+aiida_workflow = AiidaWorkGraph(core_workflow)
+output_node = aiida_workflow.run()
+
 
 
 #config[key].write_text(config[key].read_text().replace("/TESTS_ROOTDIR", str(tmp_path)))
