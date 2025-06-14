517971abd756603c27b427b83627c8da25887553
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-06-14 21:54:19 +0200

    replace everywhere remote computer label to localhost_ssh

--- ci/preparerun.py
+++ ci/preparerun.py
@@ -20,7 +20,26 @@ core_workflow = Workflow.from_config_file(str(workdir / "config.yml"))
 aiida_workflow = AiidaWorkGraph(core_workflow)
 output_node = aiida_workflow.run()
 
-
+#for 
+#    aiida_workflow.data
+#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/icon_grid_simple.nc
+#         computer: localhost_ssh
+#     - ecrad_data:
+#         type: file
+#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/ecrad_data
+#         computer: localhost_ssh
+#     - ECHAM6_CldOptProps:
+#         type: file
+#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/ECHAM6_CldOptProps.nc
+#         computer: localhost_ssh
+#     - rrtmg_sw:
+#         type: file
+#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/rrtmg_sw.nc
+#         computer: localhost_ssh
+#     - dmin_wetgrowth_lookup:
+#         type: file
+#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/dmin_wetgrowth_lookup.nc
+#
 
 #config[key].write_text(config[key].read_text().replace("/TESTS_ROOTDIR", str(tmp_path)))
 #"/capstor/store/cscs/userlab/cwd01/ricoh"
