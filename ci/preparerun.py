3eb4d32530cda01bd8d26d0e4f631a8f441c038e
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-06-15 14:26:52 +0200

    change tmp path

--- ci/preparerun.py
+++ ci/preparerun.py
@@ -92,32 +92,7 @@ with transport:
             transport.putfile(source, dest)
 
     print(transport.listdir(REMOTE_TESTSDIR))
-#for 
-#    aiida_workflow.data
-#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/icon_grid_simple.nc
-#         computer: localhost_ssh
-#     - ecrad_data:
-#         type: file
-#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/ecrad_data
-#         computer: localhost_ssh
-#     - ECHAM6_CldOptProps:
-#         type: file
-#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/ECHAM6_CldOptProps.nc
-#         computer: localhost_ssh
-#     - rrtmg_sw:
-#         type: file
-#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/rrtmg_sw.nc
-#         computer: localhost_ssh
-#     - dmin_wetgrowth_lookup:
-#         type: file
-#         src: /DATA_REMOTEDIR/tests/cases/small-icon/config/ICON/dmin_wetgrowth_lookup.nc
-#
+
 core_workflow = Workflow.from_config_file(str(workdir / "tests/cases/small-icon/config/config.yml"))
 aiida_workflow = AiidaWorkGraph(core_workflow)
 output_node = aiida_workflow.run()
-
-#config[key].write_text(config[key].read_text().replace("/TESTS_ROOTDIR", str(tmp_path)))
-#"/capstor/store/cscs/userlab/cwd01/ricoh"
-
-# real test case
-#/capstor/store/cscs/userlab/cwd01/leclairm/Sirocco_test_cases/exclaim_ape_R02B04
