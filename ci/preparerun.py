c409126703563129e33e8c9bee04a71c3edf1ca6
Alexander Goscinski <alexander.goscinski@posteo.de>
2025-06-23 12:01:30 +0200

    try to use cwd01 project dir

--- ci/preparerun.py
+++ ci/preparerun.py
@@ -47,8 +47,8 @@ if workdir.exists():
 shutil.copytree(test_case, workdir / "tests/cases/small-icon/config")
 yml_path = workdir / "tests/cases/small-icon/config/config.yml"
 
-REMOTE_TESTSDIR = "/capstor/scratch/cscs/ricoh/sirocco-tests/"
-yml_path.write_text(yml_path.read_text().replace("/TESTS_ROOTDIR/", REMOTE_TESTSDIR).replace("/DATA_REMOTEDIR/", REMOTE_TESTSDIR))
+REMOTE_TESTSDIR = "/capstor/store/cscs/userlab/cwd01/sirocco-ci"
+yml_path.(yml_path.read_text().replace("/TESTS_ROOTDIR/", REMOTE_TESTSDIR).replace("/DATA_REMOTEDIR/", REMOTE_TESTSDIR))
 print("yml", yml_path, "content:\n", yml_path.read_text())
 
 # TODO @ ali why this file cannot be put?
